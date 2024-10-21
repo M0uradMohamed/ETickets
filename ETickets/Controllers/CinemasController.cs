6c64ca274023f1b3acb378308265081a041e3f1a
M0uradMohamed <mouradmoahmed@gmail.com>
2024-10-21 23:16:06 +0300

    add cinemas controller and view

---
+++ ETickets/Controllers/CinemasController.cs
@@ -0,0 +1,21 @@
+﻿using ETickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ETickets.Controllers
+{
+    public class CinemasController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var categories = context.Cinemas.Include(e => e.Movies).ToList();
+            return View(categories);
+        }
+        public IActionResult Movies(int id)
+        {
+            var movies = context.Cinemas.Include(e => e.Movies).Where(e => e.Id == id).FirstOrDefault();
+            return View(movies);
+        }
+    }
+}
