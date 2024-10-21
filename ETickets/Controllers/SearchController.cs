92a6bdd99858107bd003d03d4dd22d2d54e51c4a
M0uradMohamed <mouradmoahmed@gmail.com>
2024-10-22 00:13:01 +0300

    add search controller and search par

---
+++ ETickets/Controllers/SearchController.cs
@@ -0,0 +1,15 @@
+﻿using ETickets.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETickets.Controllers
+{
+    public class SearchController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult Index(string name)
+        {
+            var movies = context.Movies.Where(e=>e.Name.Contains(name)).ToList();
+            return View(movies);
+        }
+    }
+}
